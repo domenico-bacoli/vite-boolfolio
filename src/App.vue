70a839b079f38cffc75e545c0fb786b9512206e4
domenicobacoli <domenico.bacoli89@gmail.com>
2023-05-29 18:43:24 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from 'axios';
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      projects: [],
+    }
+  },
+
+  created() {
+    this.getProjects();
+  },
+
+  methods: {
+    getProjects() {
+
+      axios.get('http://127.0.0.1:8000/api/projects').then(response => {
+        console.log(response.data.results);
+        this.projects = response.data.results;
+      });
+    }
+  },
+}
+
+</script>
+
+<template>
+  <div class="container pt-5">
+    <h1>I miei Progetti</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
